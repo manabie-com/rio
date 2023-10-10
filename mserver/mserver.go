47dd85f7f10a098fb50f531bbf2a0881d801ed8c
Kien Dinh <kienmatu1@gmail.com>
2023-10-10 14:38:38 +0700

    FeatL Export config

--- mserver/mserver.go
+++ mserver/mserver.go
@@ -8,6 +8,7 @@ import (
 	"github.com/manabie-com/rio/internal/config"
 	"github.com/manabie-com/rio/internal/database"
 	"github.com/manabie-com/rio/internal/log"
+	fs "github.com/manabie-com/rio/internal/storage"
 )
 
 func StartServerWithConfig(cfg *config.Config) {
@@ -21,7 +22,7 @@ func StartServerWithConfig(cfg *config.Config) {
 		panic(err)
 	}
 
-	dbConfig := config.NewDBConfig()
+	dbConfig := cfg.DB
 	if err := database.Migrate(ctx, dbConfig, "schema/migration"); err != nil {
 		panic(err)
 	}
@@ -29,5 +30,33 @@ func StartServerWithConfig(cfg *config.Config) {
 	if err := app.Start(ctx); err != nil {
 		log.Error(ctx, err)
 		panic(err)
+	}
+}
+
+// NewConfig
+// serverAddress: 0.0.0.0:8896
+func NewConfig(serverAddr, dbHost, dbName, dbSchema, dbUser, dbPassword, dbPort string) *config.Config {
+	return &config.Config{
+		ServerAddress:   serverAddr,
+		DB:              NewDBConfig(dbHost, dbName, dbSchema, dbUser, dbPassword, dbPort, 20, 2, 4),
+		FileStorageType: "local",
+		FileStorage: fs.LocalStorageConfig{
+			UseTempDir:  true,
+			StoragePath: "uploaded_files",
+		},
+	}
+}
+
+func NewDBConfig(host, dbName, dbSchema, user, password, port string, connectionLifetimeSeconds, maxIdleConnections, maxOpenConnections int) *config.PostgresConfig {
+	return &config.PostgresConfig{
+		Host:                      host,
+		DBName:                    dbName,
+		DBSchema:                  dbSchema,
+		User:                      user,
+		Password:                  password,
+		Port:                      port,
+		ConnectionLifetimeSeconds: connectionLifetimeSeconds,
+		MaxIdleConnections:        maxIdleConnections,
+		MaxOpenConnections:        maxOpenConnections,
 	}
 }
